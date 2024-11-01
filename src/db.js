3ad69894513a5f622c759400d0d2395c327259a6
KilianCPE <kilian.violet@cpe.fr>
2024-11-01 20:49:51 +0100

    change .env variable

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.POSTGRES_URI, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
 });
